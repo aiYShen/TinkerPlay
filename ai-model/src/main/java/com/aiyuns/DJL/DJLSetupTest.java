5d70a5be8d06292cd6d685fc97b70e695edb2d0f
yuxinbai <784724280@qq.com>
2025-02-23 22:28:10 +0800

    DJL

---
+++ ai-model/src/main/java/com/aiyuns/DJL/DJLSetupTest.java
@@ -0,0 +1,12 @@
+package com.aiyuns.DJL;
+
+import ai.djl.engine.Engine;
+
+public class DJLSetupTest {
+
+    public static void main(String[] args) {
+        System.out.println("Default Engine: " + Engine.getDefaultEngineName());
+        System.out.println("Available Engines: " + Engine.getAllEngines());
+    }
+
+}
