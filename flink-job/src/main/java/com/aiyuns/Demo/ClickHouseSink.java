705930f7466afaa4d229d811530319412fcc4cc9
yuxinbai <784724280@qq.com>
2025-05-26 15:13:18 +0800

    flink java demo

---
+++ flink-job/src/main/java/com/aiyuns/Demo/ClickHouseSink.java
@@ -0,0 +1,41 @@
+package com.aiyuns.Demo;
+
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class ClickHouseSink extends RichSinkFunction<String> {
+
+    private transient Connection connection;
+    private transient PreparedStatement preparedStatement;
+    private ScheduledExecutorService scheduledExecutorService;
+
+    // 初始化 Sink 的生命周期方法，在 Sink 任务启动时调用: 开启一些东西, 例如打开数据库jdbc连接
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+
+        // 启动一些定时任务: 例如攒批提交
+        scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        Runnable runnable = () -> {
+            
+        };
+    }
+
+    // 在 Sink 任务结束时调用，用于清理资源: 关闭一些东西, 例如关闭数据库jdbc连接
+    @Override
+    public void close() throws Exception {
+        super.close();
+    }
+
+    // 处理每条输入记录的核心方法: 业务逻辑处理代码
+    @Override
+    public void invoke(String value, Context context) throws Exception {
+        super.invoke(value, context);
+    }
+
+}
